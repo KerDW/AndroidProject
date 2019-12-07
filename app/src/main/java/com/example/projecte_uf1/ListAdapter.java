454065b23eb130a757859216328522437d346f71
Álvaro Zamora <alvarozajim@gmail.com>
2019-12-07 15:57:22 +0100

    refactoring

--- app/src/main/java/com/example/projecte_uf1/ListAdapter.java
+++ app/src/main/java/com/example/projecte_uf1/ListAdapter.java
@@ -14,35 +14,30 @@ import java.util.List;
 
 class ListAdapter extends ArrayAdapter <User>{
 
-    Context ctx;
-    int plantillaLayout;
+    Context c;
+    int layout;
     List<User> users;
 
     public ListAdapter(@NonNull Context context, int resource, @NonNull List<User> objects) {
         super(context, resource, objects);
 
-        this.ctx = context;
-        this.plantillaLayout = resource;
+        this.c = context;
+        this.layout = resource;
         this.users = objects;
     }
 
-    //Aquest mètode es llança automaticament cada vegada per element
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        //parent: el carga dins de l'elemnt pare, el listView que rep de l'activity
-        View v = LayoutInflater.from(ctx).inflate(plantillaLayout,parent,false);
+        View v = LayoutInflater.from(c).inflate(layout,parent,false);
 
-        //Obtenim el elements de la llista
         User u = users.get(position);
 
-        //Rescatem els elements de la IU de la plantillaLayout
         TextView pos = v.findViewById(R.id.position);
         TextView name = v.findViewById(R.id.user_name);
         TextView time = v.findViewById(R.id.user_time);
 
-        //Fem un set de la info de l'element actual
         pos.setText(position+1+"");
         name.setText(u.getName());
         time.setText(String.format("%.3f",u.getTime()/1000)+"s");
--- app/src/main/java/com/example/projecte_uf1/ThirdActivity.java
+++ app/src/main/java/com/example/projecte_uf1/ThirdActivity.java
@@ -77,6 +77,4 @@ public class ThirdActivity extends AppCompatActivity {
 
     }
 
-
-
 }
