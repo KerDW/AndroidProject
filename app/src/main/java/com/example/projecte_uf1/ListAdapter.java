06ea6addc0adde953a998c2245342fccd5e9eac8
Álvaro Zamora <alvarozajim@gmail.com>
2019-12-08 02:25:26 +0100

    implicit intent whatsapp share score

--- app/src/main/java/com/example/projecte_uf1/ListAdapter.java
+++ app/src/main/java/com/example/projecte_uf1/ListAdapter.java
@@ -1,11 +1,16 @@
 package com.example.projecte_uf1;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,15 +37,40 @@ class ListAdapter extends ArrayAdapter <User>{
 
         View v = LayoutInflater.from(c).inflate(layout,parent,false);
 
-        User u = users.get(position);
+        final User u = users.get(position);
 
         TextView pos = v.findViewById(R.id.position);
         TextView name = v.findViewById(R.id.user_name);
         TextView time = v.findViewById(R.id.user_time);
+        ImageView share = v.findViewById(R.id.shareScore);
 
         pos.setText(position+1+"");
         name.setText(u.getName());
         time.setText(String.format("%.3f",u.getTime()/1000)+"s");
+
+        share.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                PackageManager pm = c.getPackageManager();
+                try {
+
+                    Intent waIntent = new Intent(Intent.ACTION_SEND);
+                    waIntent.setType("text/plain");
+                    String text = "Check out my time in this try: "+u.getTime()/1000+"s!";
+
+                    PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+                    waIntent.setPackage("com.whatsapp");
+
+                    waIntent.putExtra(Intent.EXTRA_TEXT, text);
+                    c.startActivity(Intent.createChooser(waIntent, "Share with"));
+
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(c, "WhatsApp not found, unable to share score.", Toast.LENGTH_LONG)
+                            .show();
+                }
+
+            }
+        });
 
         return v;
 
