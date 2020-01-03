d7d02371cad37fd9d921fec0f7e93fadc45c6fd7
Álvaro Zamora <alvarozajim@gmail.com>
2020-01-03 16:57:38 +0100

    adjusted fragmentscomm class

--- app/src/main/java/com/example/projecte_uf1/FragmentsComm.java
+++ app/src/main/java/com/example/projecte_uf1/FragmentsComm.java
@@ -1,19 +1,19 @@
 package com.example.projecte_uf1;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class FragmentsComm {
 
-    private static Dimensions dimensions;
+    // I got these values using a viewtreeobserver on the fragments to get measurements, then adjusted them
+    private static Dimensions dimensions = new Dimensions(992.68f, 1515.28f);
+
     private static ArrayList<Dimensions> checkboxesPositions = new ArrayList<>();
     private static int counter = 0;
-
-    public static void setWidthHeight(Dimensions d){
-        dimensions = d;
-    }
 
     public static Dimensions getRandomDimensions(){
 
@@ -32,7 +32,7 @@ public class FragmentsComm {
             dy = rand.nextFloat() * dimensions.getHeight();
             for (int i = 0; i < checkboxesPositions.size(); i++) {
                 if((dx >= checkboxesPositions.get(i).getWidth()-separation && dx <= checkboxesPositions.get(i).getWidth()+separation) && (dy >= checkboxesPositions.get(i).getHeight()-separation && dy <= checkboxesPositions.get(i).getHeight()+separation)){
-                    Log.e("xd", "xd");
+                    Log.e("xd", "collision avoided");
                     areDistanced = false;
                     break;
                 }
@@ -41,7 +41,7 @@ public class FragmentsComm {
 
         Dimensions newDimensions = new Dimensions(dx, dy);
 
-        if(counter == 5){
+        if(counter == Difficulty.getCheckBoxNo()){
             counter = 0;
             checkboxesPositions.clear();
         }
